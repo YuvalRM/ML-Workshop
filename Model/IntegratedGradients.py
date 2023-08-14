134f65b3e2aa4c7460b6da93771ddc478bd8e9a3
Yuval Ramot <81155181+YuvalRM@users.noreply.github.com>
2023-08-14 11:04:09 +0300

    Update IntegratedGradients.py

--- Model/IntegratedGradients.py
+++ Model/IntegratedGradients.py
@@ -12,17 +12,28 @@ test_data = np.transpose(test_data, (0, 2, 1))  # (455024, 4, 1000)
 test_data = np.expand_dims(test_data, axis=2)  # (455024, 4, 1, 1000)
 test_dataset = TensorDataset(torch.from_numpy(test_data).float(), torch.from_numpy(test_labels).float())
 # defining model input tensors
+
 input1=test_data[0]
 input1=np.expand_dims(input1, axis=0)
-input1 = torch.tensor(input1)
+input1 = torch.tensor(input1).float()
 
 # defining baselines for each input tensor
 baseline1 = torch.rand(1,4,1,1000)
 
-print(input1.shape)
 # defining and applying integrated gradients on ToyModel and the
 ig = IntegratedGradients(model)
 attributions, approximation_error = ig.attribute((input1),
                                                  baselines=(baseline1),
                                                  method='gausslegendre',
-                                                 return_convergence_delta=True)
+                                                 return_convergence_delta=True,
+                                                 target=[0])
+print(attributions)
+print(attributions.shape)
+attributions, approximation_error = ig.attribute((input1),
+                                                 baselines=(baseline1),
+                                                 method='gausslegendre',
+                                                 return_convergence_delta=True,
+                                                 target=[1])
+
+print(attributions)
+print(attributions.shape)
