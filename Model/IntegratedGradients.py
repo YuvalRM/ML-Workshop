b825205cc6eed9536e9e33e530783b99304f7f50
eshedgal1 <142097407+eshedgal1@users.noreply.github.com>
2023-08-17 16:38:11 +0300

    Add files via upload

--- Model/IntegratedGradients.py
+++ Model/IntegratedGradients.py
@@ -69,3 +69,51 @@ def calc_IG():
 
     # return input_lst, weights_lst
     return input_lst, weight_dict
+
+
+# this function calculate the integrated gradients for one feature, on many samples
+
+def calc_IG_mult_samples():
+    input_lst = []
+    weights_lst = []
+    for i in range(len(test_data)):
+        # defining model input tensors
+        input1 = test_data[i]
+        input1 = np.expand_dims(input1, axis=0)
+        input1 = torch.tensor(input1).float()
+
+        # defining baselines for each input tensor
+        baseline1 = torch.rand(1, 4, 1, 1000)
+
+        # defining and applying integrated gradients on ToyModel and the
+        ig = IntegratedGradients(model)
+        attributions, approximation_error = ig.attribute((input1),
+                                                         baselines=(baseline1),
+                                                         method='gausslegendre',
+                                                         return_convergence_delta=True,
+                                                         target=[0])
+
+        three_d_tensor = attributions[0]
+        three_d_tensor = torch.transpose(three_d_tensor, 0, 1)
+        three_d_tensor = torch.transpose(three_d_tensor, 1, 2)
+
+        three_d_input = input1.int()[0]
+        three_d_input = torch.transpose(three_d_input, 0, 1)
+        three_d_input = torch.transpose(three_d_input, 1, 2)
+
+
+        # create list with input data on right dimentions (one-hot vectors)
+        two_d_input = three_d_input[0]
+        input_lst.append(two_d_input.numpy())
+
+        # create list with attributions data on right dimentions
+        two_d_input = three_d_tensor[0]
+        weights_lst.append(two_d_input.numpy())
+
+        print("visited IG function")
+
+    # creates the required dict
+    weight_dict = {'task': weights_lst}
+
+    # return input_lst, weights_lst
+    return input_lst, weight_dict
