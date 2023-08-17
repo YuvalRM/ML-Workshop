07370d557fadf52a991e8ad759a50f78449dc0ed
eshedgal1 <142097407+eshedgal1@users.noreply.github.com>
2023-08-17 17:33:18 +0300

    Add files via upload

--- Model/IntegratedGradients.py
+++ Model/IntegratedGradients.py
@@ -2,6 +2,7 @@ from captum.attr import IntegratedGradients
 import torch
 from torch.utils.data import Dataset, DataLoader, TensorDataset
 import deepsea_model
+# import hypo_scores
 import  numpy as np
 data_dir = "data_dir/"
 
@@ -25,7 +26,6 @@ def calc_IG():
     # defining baselines for each input tensor
     baseline1 = torch.rand(1, 4, 1, 1000)
 
-    # defining and applying integrated gradients on ToyModel and the
     ig = IntegratedGradients(model)
     attributions, approximation_error = ig.attribute((input1),
                                                      baselines=(baseline1),
@@ -62,7 +62,7 @@ def calc_IG():
     weights_lst.append(two_d_input.numpy())
     # print("two_d_input shape is", weights_lst) # 1, 1000, 4 as wanted
 
-    print("visited IG function")
+    print("visited IG function for one sample one feature")
 
     # creates the required dict
     weight_dict = {'task': weights_lst}
@@ -71,8 +71,69 @@ def calc_IG():
     return input_lst, weight_dict
 
 
-# this function calculate the integrated gradients for one feature, on many samples
+# this function calculate the integrated gradients with hypo, one sample one feature
+def calc_IG_with_hypo():
+    # defining model input tensors
+    input1 = test_data[0]
+    input1 = np.expand_dims(input1, axis=0)
+    input1 = torch.tensor(input1).float()
 
+    # defining baselines for each input tensor
+    baseline1 = torch.rand(1, 4, 1, 1000)
+
+    ig = IntegratedGradients(model)
+    attributions, approximation_error = ig.attribute((input1),
+                                                     baselines=(baseline1),
+                                                     method='gausslegendre',
+                                                     return_convergence_delta=True,
+                                                     target=[0])
+
+
+    three_d_tensor = attributions[0]
+    three_d_tensor = torch.transpose(three_d_tensor, 0, 1)
+    three_d_tensor = torch.transpose(three_d_tensor, 1, 2)
+
+    three_d_input = input1.int()[0]
+    three_d_input = torch.transpose(three_d_input, 0, 1)
+    three_d_input = torch.transpose(three_d_input, 1, 2)
+
+    # create list with input data on right dimentions (one-hot vectors)
+    two_d_input = three_d_input[0]
+    input_lst = []
+    input_lst.append(two_d_input.numpy())
+
+    # create list with attributions data on right dimentions
+    two_d_tensor = three_d_tensor[0]
+    weights_lst = []
+    weights_lst.append(two_d_tensor.numpy())
+
+    print("visited IG_with_hypo function for one sample one feature")
+
+    # creates the required scores  dict
+    weight_dict = {'task': weights_lst}
+
+    # this part will calculate hypo scores in similar way
+    data_to_hypo = hypo_scores.calc_hypo_on_data(test_data)
+    hypo_input_1 = data_to_hypo[0]
+    hypo_input_1 = np.expand_dims(hypo_input_1, axis=0)
+    hypo_input_1 = torch.tensor(hypo_input_1).float()
+    hypo_attributions, hypo_approximation_error = ig.attribute((hypo_input_1),
+                                                     baselines=(baseline1),
+                                                     method='gausslegendre',
+                                                     return_convergence_delta=True,
+                                                     target=[0])
+    hypo_three_d_tensor = hypo_attributions[0]
+    hypo_three_d_tensor = torch.transpose(hypo_three_d_tensor, 0, 1)
+    hypo_three_d_tensor = torch.transpose(hypo_three_d_tensor, 1, 2)
+    hypo_two_d_tensor = hypo_three_d_tensor[0]
+    hypo_weights_lst = []
+    hypo_weights_lst.append(hypo_two_d_tensor.numpy())
+    hypo_weight_dict = {'task': hypo_weights_lst}
+
+    return input_lst, weight_dict, hypo_weight_dict
+
+
+# this function calculate the integrated gradients for one feature, on many samples
 def calc_IG_mult_samples():
     input_lst = []
     weights_lst = []
@@ -85,7 +146,6 @@ def calc_IG_mult_samples():
         # defining baselines for each input tensor
         baseline1 = torch.rand(1, 4, 1, 1000)
 
-        # defining and applying integrated gradients on ToyModel and the
         ig = IntegratedGradients(model)
         attributions, approximation_error = ig.attribute((input1),
                                                          baselines=(baseline1),
